a1acfb2096ed87be9551cf79fb2f340c5f99e9ed
john smith <example@standford.edu>
2013-07-15 17:58:31 +0000

    Updated web.js to look at index.html

--- web.js
+++ web.js
@@ -2,9 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+/* app.get('/', function(request, response) {
   response.send('Hello World 2!');
-});
+}); */
+
+var fs = require('fs');
+var newBuff = fs.readFileSync('index.html');
+newBuff.toString();
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
