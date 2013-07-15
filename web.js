ad8c6f027f348599569b5db4b253e64663cf0fb5
john smith <example@standford.edu>
2013-07-15 18:25:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer();
-  response.send(buffer.toString('utf8',fs.readFileSync('index.html')));
+  buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString('utf8'));
 });
 
 
